6e5b92be35a50e149fd2980e40efb071e22da615
PrudhviMV <prudhvimenta@gmail.com>
2026-05-21 12:35:04 +0530

    07 variables

--- 07-variables.sh
+++ 07-variables.sh
@@ -2,9 +2,9 @@
 
 echo "Today's date is $date"
 
-START_TIME=${date +%s}
-END_TIME=${date +%s}
+START_TIME=$(date +%s)
+END_TIME=$(date +%s)
 
-TOTAL_TIME=${($START_TIME-$END_TIME)}
+TOTAL_TIME=$(($START_TIME-$END_TIME))
 
 echo "Time took to execute script is $TOTAL_TIME"
