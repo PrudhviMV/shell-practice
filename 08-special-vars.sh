8196b0400e78da0c274a25e30756afb6ab064110
PrudhviMV <prudhvimenta@gmail.com>
2026-05-21 15:35:59 +0530

    08-special-vars

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -7,5 +7,5 @@ echo "Script name is $0"
 echo "Current Directory is $PWD"
 echo "This user is running script $USER"
 echo "PID of this script is $$"
-sleep &
+sleep 50 &
 echo "PID of last background command is $!"
