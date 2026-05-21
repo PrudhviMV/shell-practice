78a7f409edeb83c6ee9c795e72c33e18b48061c2
PrudhviMV <prudhvimenta@gmail.com>
2026-05-21 15:28:56 +0530

    08-special-vars

---
+++ 08-special-vars.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Print all variables : $@"
+echo "Print all variables : $*"
+echo "Home directory of user is $home"
+echo "Script name is $0"
+echo "This user is running script $user"
