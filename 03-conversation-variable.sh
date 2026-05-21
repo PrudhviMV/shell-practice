1d40e446fa7d8b0536022f4b7e1aa55e3ab22667
PrudhviMV <prudhvimenta@gmail.com>
2026-05-21 10:25:16 +0530

    variable conversation

---
+++ 03-conversation-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Bob
+PERSON2=Alice
+
+echo "$PERSON1: Hi $PERSON2, How are you?"
+echo "$PERSON2: Hi $PERSON1, I am good how are you?"
+echo "$PERSON1: I am good what are you learning recently?"
+echo "$PERSON2: I ve started shell scripting."
