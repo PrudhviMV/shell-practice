32c8016046a67a98714c0e33c12cac6e5c3c6fce
PrudhviMV <prudhvimenta@gmail.com>
2026-05-21 10:22:00 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: Hi Suresh, How are you?"
+echo "Suresh: Hi Ramesh, I am good how are you?"
+echo "Ramesh: I am good what are you learning recently?"
+echo "Suresh: I ve started shell scripting."
