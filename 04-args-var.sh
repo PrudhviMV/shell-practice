bddf7b41a8ca928db491cdbb20c114fc911271de
PrudhviMV <prudhvimenta@gmail.com>
2026-05-21 10:42:06 +0530

    args variables

---
+++ 04-args-var.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "PERSON1:: Welcome to White house PERSON2."
+echo "PERSON2:: Thank you! Not on zoom anymore."
+echo "PERSON1:: Can we have deal?"
+echo "PERSON2:: Yes, If you didn't impose any sanctions on india."
