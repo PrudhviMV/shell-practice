af860351e42c1cf5f3733bfa549b49bba7f3ee1b
PrudhviMV <prudhvimenta@gmail.com>
2026-05-22 09:01:42 +0530

    12 install

--- 12-installation.sh
+++ 12-installation.sh
@@ -2,8 +2,9 @@
 
 check_root=$(id -u)
 
-if [ $check_root -eq 0 ]; then
-    echo "User has root privileage, Hence proceeding with script."
+if [ $check_root -ne 0 ]; then
+    echo "User doesn't has root privileage, Hence not proceeding with script."
+    echo "Run this script with root user"
     exit 1
 fi
 
