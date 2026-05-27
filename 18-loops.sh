7b08665b04a28ddc142bd119308fcd49a6af212b
PrudhviMV <prudhvimenta@gmail.com>
2026-05-27 18:28:47 +0530

    18 loop install

---
+++ 18-loops.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#colour codes
+Bk="\e[30m"	
+R="\e[31m"	
+G="\e[32m"	
+Y="\e[33m"	
+B="\e[34m"	
+P="\e[35m"	
+C="\e[36m"	
+W="\e[37m"	
+
+check_root=$(id -u)
+
+if [ $check_root -ne 0 ]; then
+    echo "User doesn't has root privileage, Hence not proceeding with script."
+    echo "Run this script with root user"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]; then
+        echo "$2 installation successful."
+    else
+        echo "$2 installation failed."
+    fi
+}
+
+# $@ -> consider all args
+
+for package in $@
+do
+    echo $package
+done
